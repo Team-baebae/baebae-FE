f207e9b4855f1810a1470e84df1038a5c88bf8d8
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-04-18 13:48:25 +0900

    [feat] rem 기준변경 코드 삭제 및 양쪽 여백을 두고 가운데 390px로 고정하여 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
 import Router from './Router'
-import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    const baseWidth = 390 // 기준 너비
-    const baseFontSize = 16 // 기준 폰트 크기
-
-    function handleResize() {
-      const scaleFactor = window.innerWidth / baseWidth
-      const newFontSize = baseFontSize * scaleFactor
-      document.documentElement.style.fontSize = `${newFontSize}px`
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    // 컴포넌트 마운트 시 초기 폰트 크기 설정
-    handleResize()
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
   return <Router />
 }
 
--- src/pages/Outer.tsx
+++ src/pages/Outer.tsx
@@ -3,14 +3,26 @@ import { Outlet } from 'react-router-dom'
 
 const Outer = () => {
   return (
-    <LandingComponent>
-      <Outlet />
-    </LandingComponent>
+    <TotalBrowserComponent>
+      <PhoneBrowserComponent>
+        <Outlet />
+      </PhoneBrowserComponent>
+    </TotalBrowserComponent>
   )
 }
 
 export default Outer
 
-const LandingComponent = styled.div`
+const TotalBrowserComponent = styled.div`
+  display: flex;
+  justify-content: center;
   width: 100vw;
+  min-height: 100vh;
+`
+
+const PhoneBrowserComponent = styled.div`
+  width: 390px;
+  min-height: 100%;
+  /* 경계 지을려고 일단 이렇게 설정함 추후 수정 예정 */
+  background-color: #f1f1f1;
 `
