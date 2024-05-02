e8fb80a72f064077e4365dc3c27f639e951108d0
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-02 20:17:01 +0900

    [bug] 회원가입 된 계정인지 파악하는 과정 추가후 로직 변경

--- src/pages/KakaoRedirection.tsx
+++ src/pages/KakaoRedirection.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-import { getKakaoUserInfoApi, loginApi } from '../apis/UserApi'
+import { getKakaoUserInfoApi, isExistingAccountApi, loginApi } from '../apis/UserApi'
 import loading from '../assets/kakaoRedirection/Loading.svg'
 
 const KakaoRedirection = () => {
@@ -23,11 +23,65 @@ const KakaoRedirection = () => {
     try {
       await getKakaoUserInfoApi(code).then((res: KakaoUserInfoResponseProps) => {
         if (res.status === 200) {
+          isExistingAccount(res.data.access_token)
+        } else {
+          alert('로그인 실패')
+          navigate('/login')
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // GetKakaoUserInfo함수의 response 인터페이스
+  interface isExistingAccountResponseProps {
+    data: any
+    status: number
+    statusText: string
+    headers: any
+    config: any
+  }
+
+  // 이미 존재하는 회원인지 여부 확인(여기 accessToken은 카카오어세스토큰)
+  const isExistingAccount = async (accessToken: string) => {
+    try {
+      await isExistingAccountApi(accessToken).then((res: isExistingAccountResponseProps) => {
+        if (res.data.isExisting) {
+          login(accessToken, 'null')
+        } else {
           navigate('/signup', {
             state: {
-              kakaoAccessToken: res.data.access_token,
+              kakaoAccessToken: accessToken,
             },
           })
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // Login함수의 response 인터페이스
+  interface LoginProps {
+    data: any
+    status: number
+    statusText: string
+    headers: any
+    config: any
+  }
+
+  // 이미 존재하는 회원일 경우 바로 로그인 진행
+  const login = async (kakaoAccessToken: string, nickname: string) => {
+    try {
+      await loginApi(kakaoAccessToken, nickname).then((res: LoginProps) => {
+        if (res.status === 200) {
+          localStorage.setItem('accessToken', res.data.accessToken)
+          localStorage.setItem('nickname', res.data.nickname)
+          localStorage.setItem('email', res.data.email)
+          localStorage.setItem('refreshToken', res.data.refreshToken)
+
+          navigate('/')
         } else {
           alert('로그인 실패')
           navigate('/login')
