7823afbe056de6f45bb0cb0451925cacb6b49e6c
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-03 14:19:14 +0900

    [Design] 로그인 디자인 변경

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import loginBackground from '../assets/login/LoginBackround.svg'
+import logo from '../assets/Logo.svg'
 
 const Login = () => {
   const clientId = import.meta.env.VITE_KAKAO_CLIENT_ID
@@ -11,7 +13,8 @@ const Login = () => {
 
   return (
     <Container>
-      <FlipItIcon>로고</FlipItIcon>
+      <BackgroundImg src={loginBackground} alt="back" />
+      <FlipItLogo src={logo} alt="logo" />
       <KakaoLoginBtn onClick={loginHandler}>카카오 로그인</KakaoLoginBtn>
     </Container>
   )
@@ -21,28 +24,38 @@ export default Login
 
 const Container = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #3b3b3b;
 `
 
-const FlipItIcon = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 264px;
-  height: 264px;
-  border-radius: 30px;
-  background-color: #d9d9d9;
-  margin: 202.5px 0px 0px 0px;
+const BackgroundImg = styled.img`
+  margin: 21.04px 0px 0px 0px;
+  width: 527.03px;
+  height: 431.605px;
+  position: absolute;
+  left: -86px;
+  top: 21.035px;
+  object-fit: cover;
+`
+
+const FlipItLogo = styled.img`
+  width: 200.001px;
+  height: 89.743px;
+  margin: 250px 0px 0px 0px;
 `
 
 const KakaoLoginBtn = styled.button`
-  width: 335px;
+  display: flex;
   height: 56px;
+  margin: 40px 20px 0px 20px;
+  padding: 16px 67px;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
   border-radius: 12px;
-  background-color: #fee500;
-  border: none;
-  margin: 40px 0px 0px 0px;
+  background: #fee500;
   cursor: pointer;
 `
