6bde56bda69eab751f6538bd75d1aa962a3f4a9f
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-03 16:39:39 +0900

    [rename] SignUp 파일이름 SignUpNickname으로 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,10 +4,10 @@ import Landing from './pages/Landing'
 import Login from './pages/Login'
 import KakaoRedirection from './pages/KakaoRedirection'
 import Spotify from './pages/Spotify'
-import SignUp from './pages/SignUp'
+import SignUpTerms from './pages/SignUpTerms'
+import SignUpNickname from './pages/SignUpNickname'
 import Setting from './pages/Setting'
 import EditProfile from './pages/EditProfile'
-import SignUpTerms from './pages/SignUpTerms'
 
 const Router = () => {
   return (
@@ -16,9 +16,9 @@ const Router = () => {
         <Route path="/" element={<Outer />}>
           <Route path="" element={<Landing />} />
           <Route path="login" element={<Login />} />
-          <Route path="signup/terms" element={<SignUpTerms />} />
           <Route path="auth" element={<KakaoRedirection />} />
-          <Route path="signup" element={<SignUp />} />
+          <Route path="signup/terms" element={<SignUpTerms />} />
+          <Route path="signup/nickname" element={<SignUpNickname />} />
           <Route path="spotify" element={<Spotify />} />
           <Route path="setting" element={<Setting />} />
           <Route path="editProfile" element={<EditProfile />} />
