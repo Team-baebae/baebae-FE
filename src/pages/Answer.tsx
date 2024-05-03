488043708119833e454189a9692ac8a259c353f4
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-04 02:32:46 +0900

    [feat] 답변페이지 질문 ui 구현

--- src/pages/Answer.tsx
+++ src/pages/Answer.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 import Header from '../components/common/Header'
 import { colors } from '../styles/colors'
+import Question from '../components/answer/Question'
 
 const Answer = () => {
   return (
     <Container>
       <Header text="답변하기" backColor={colors.grey7} />
+      <Question />
     </Container>
   )
 }
@@ -16,4 +18,5 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `
