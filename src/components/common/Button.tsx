d5ef8c5f15a681ce5f267dc9254ad27fab16379b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-05 16:07:34 +0900

    [fix] 버튼 공통 컴포넌트 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,19 +4,21 @@ import { colors } from '../../styles/colors'
 interface ButtonProps {
   positive: boolean
   func: any
+  text: string
 }
 
-interface UnFixedButtonProps {
-  margin: string
+interface UnFixedButtonprops {
   positive: boolean
   func: any
+  text: string
+  margin: string
 }
 
 // 공통 초록색 버튼 구현 (positive가 true일경우 진한초록, false일경우 연한초록)
-export const FixedButton = ({ positive, func }: ButtonProps) => {
+const Button = ({ positive, func, text }: ButtonProps) => {
   return positive ? (
     <GreenCommonBtn onClick={func} positive={true}>
-      다음
+      {text}
     </GreenCommonBtn>
   ) : (
     <GreenCommonBtn
@@ -25,16 +27,54 @@ export const FixedButton = ({ positive, func }: ButtonProps) => {
       }}
       positive={false}
     >
-      다음
+      {text}
     </GreenCommonBtn>
   )
 }
 
+export default Button
+
+const GreenCommonBtn = styled.button<{ positive: boolean }>`
+  @media screen and (width <= 768px) {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    left: 20px;
+    right: 20px;
+    bottom: 30px;
+    border: none;
+    border-radius: 12px;
+    padding: 16px 20px;
+    background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
+    color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
+    font-family: Pretendard;
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+  @media screen and (width > 768px) {
+    position: absolute;
+    bottom: 30px;
+    display: flex;
+    justify-content: center;
+    width: 335px;
+    border: none;
+    border-radius: 12px;
+    padding: 16px 20px;
+    background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
+    color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
+    font-family: Pretendard;
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`
+
 // 공통 초록색 버튼 구현 (하단 고정되지 않은 버전)
-export const UnFixedButton = ({ margin, positive, func }: UnFixedButtonProps) => {
+export const UnFixedButton = ({ positive, func, text, margin }: UnFixedButtonprops) => {
   return positive ? (
     <UnFixedGreenCommonBtn margin={margin} onClick={func} positive={true}>
-      다음
+      {text}
     </UnFixedGreenCommonBtn>
   ) : (
     <UnFixedGreenCommonBtn
@@ -44,55 +84,39 @@ export const UnFixedButton = ({ margin, positive, func }: UnFixedButtonProps) =>
       }}
       positive={false}
     >
-      다음
+      {text}
     </UnFixedGreenCommonBtn>
   )
 }
 
-const GreenCommonBtn = styled.button<{ positive: boolean }>`
-  position: absolute;
-  bottom: 30px;
-  width: 335px;
-  display: flex;
-  height: 56px;
-  padding: 16px 20px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-  background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
-  color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
-  font-family: Pretendard;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 150%;
-  letter-spacing: -0.28px;
-  border: none;
-  cursor: pointer;
-  @media screen and (max-width: 768px) {
-    width: calc(100% - 40px);
-  }
-`
 const UnFixedGreenCommonBtn = styled.button<{ margin: string; positive: boolean }>`
-  margin: ${(props) => props.margin || '0px 20px 0px 20px'};
-  width: 335px;
-  display: flex;
-  height: 56px;
-  padding: 16px 20px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-  background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
-  color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
-  font-family: Pretendard;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 150%;
-  letter-spacing: -0.28px;
-  border: none;
-  cursor: pointer;
-  @media screen and (max-width: 768px) {
-    width: calc(100% - 40px);
+  @media screen and (width <= 768px) {
+    display: flex;
+    justify-content: center;
+    margin: ${(props) => props.margin};
+    border: none;
+    border-radius: 12px;
+    padding: 16px 20px;
+    background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
+    color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
+    font-family: Pretendard;
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+  @media screen and (width > 768px) {
+    margin: ${(props) => props.margin};
+    display: flex;
+    justify-content: center;
+    width: 335px;
+    border: none;
+    border-radius: 12px;
+    padding: 16px 20px;
+    background: ${(props) => (props.positive ? colors.primary : colors.primary40)};
+    color: ${(props) => (props.positive ? colors.grey1 : colors.grey3)};
+    font-family: Pretendard;
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
   }
 `
