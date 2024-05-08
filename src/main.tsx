f77bf04c0bacf4f2c6da86f163bf31c1b85b479b
HIHJH <2170095@ewhain.net>
2024-05-08 14:05:39 +0900

    [fix] 폰트 파일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
+import './fonts/fonts.css'
 import GlobalStyle from './styles/GlobalStyles.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
