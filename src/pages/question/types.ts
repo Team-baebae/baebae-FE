f81aa0c2598428cd07f74e350476b0017e9a4aab
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-14 21:44:48 +0900

    [fix] 답변 기다리는 질문 페이지 코드 수정

--- src/pages/question/types.ts
+++
@@ -1,9 +0,0 @@
-export interface QuestionProps {
-  questionId: number
-  content: string
-  nickname: string
-  profileOnOff: boolean
-  createdDate: string
-  isAnswered: boolean
-  fcmtoken: string
-}
